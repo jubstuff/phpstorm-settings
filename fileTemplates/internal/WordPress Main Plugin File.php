5d38f20ad9b061c2a112f24b41a60c52226a7ec3
Giustino Borzacchiello <giustinob@gmail.com>
2018-11-18 19:26:05 +0100

    PS-2018.2.5 <giustino@MK249 Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, project.default.xml, Default copy.xml, other.xml, Default.xml, keymap.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, php.xml, colors.scheme.xml, git.xml, databaseDrivers.xml, stringManipulation.xml, PHP Getter Method.php, Default.xml, laf.xml, vcs.xml, editor.codeinsight.xml, PHP Setter Method.php, databaseSettings.xml, WordPress Main Plugin File.php, editor.xml, uml.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/internal/WordPress Main Plugin File.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+Plugin Name: ${PLUGIN_NAME}
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: ${USER}
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+
+
+<?php
+/**
+ * Plugin Name: ${PLUGIN_NAME}
+ * Description: 
+ * Plugin URI:  
+ * Author:      Giustino Borzacchiello
+ * Author URI:  https://giustino.blog/
+ * Text Domain: 
+ * Domain Path: /languages
+ * Version:     0.1.0
+ *
+ */
+
+/*
+ * Copyright (c) 2018 Giustino Borzacchiello (https://giustino.blog)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
